eb91fd0533fd65e014b241afca6c212e8b0d393f
Karolina Jelen <jehonala@gmail.com>
2022-09-05 21:40:39 +0200

    Add translation of captions into lolspeak

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/zinah/memegen/pictures"
 	"github.com/zinah/memegen/captions"
+	"github.com/zinah/memegen/pictures"
+	"github.com/zinah/memegen/translation"
 	"github.com/zinah/memegen/web"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	textAbove, textBelow := captions.GetJoke()
+	dictionary := translation.GetDictionaryFromJson("./assets/tranzlashun.json")
+	text1, text2 := captions.GetJoke()
+	textAbove, textBelow := translation.Translate(text1, dictionary), translation.Translate(text2, dictionary)
 	if bgImage, err := pictures.GetImage("https://cataas.com/cat", "./assets/default_cat.jpeg"); err != nil {
 		web.GetErrorResponse(w, err)
 	} else {
---
+++ translation/main.go
@@ -0,0 +1,39 @@
+package translation
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+// TODO dictionary should be in some sort of DB instead
+func Translate(text string, dictionary map[string]string) (string) {
+	// TODO handle punctuation, especially at the end
+	// TODO handle empty text?
+	tokens := strings.Split(text, " ")
+	// TODO Should I use make with len and cap of tokens here instead and fill it, not append to it?
+	var translatedText []string
+	for _, word := range tokens {
+		translatedWord := dictionary[strings.ToLower(word)]
+		if translatedWord != "" {
+			translatedText = append(translatedText, translatedWord)
+		} else {
+			translatedText = append(translatedText, word)
+		}
+	}
+	return strings.Join(translatedText, " ")
+}
+	
+func GetDictionaryFromJson(path string) (map[string]string) {
+	jsonDictionary, err := ioutil.ReadFile(path)
+    if err != nil {
+        log.Fatal("Error when opening file: ", err)
+	}
+
+	var dictionary map[string]string
+	// TODO handle errors when JSON malformed
+	json.Unmarshal([]byte(jsonDictionary), &dictionary)
+
+	return dictionary
+}
