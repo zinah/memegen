3019053ee7e8819c8d886713b0e726970baa6036
Karolina Jelen <jehonala@gmail.com>
2022-09-05 22:30:17 +0200

    Use configuration file

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.19
 
 require github.com/fogleman/gg v1.3.0
 
+require github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f
+
 require (
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"github.com/tkanos/gonfig"
 	"github.com/zinah/memegen/captions"
 	"github.com/zinah/memegen/pictures"
 	"github.com/zinah/memegen/translation"
 	"github.com/zinah/memegen/web"
 )
 
+type Configuration struct {
+	ImageSourceURL string
+	DefaultImageLocalPath string
+	DictionaryLocalPath string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	dictionary := translation.GetDictionaryFromJson("./assets/tranzlashun.json")
+	// TODO Do not to load this on every request - how to pass the config into the handler?
+	configuration := Configuration{}
+	err := gonfig.GetConf("dev_config.json", &configuration)
+	if err != nil {
+		panic(err)
+	}
+	// TODO Don't load the dictionary every time a request is made, it doesn't change
+	dictionary := translation.GetDictionaryFromJson(configuration.DictionaryLocalPath)
 	text1, text2 := captions.GetJoke()
 	textAbove, textBelow := translation.Translate(text1, dictionary), translation.Translate(text2, dictionary)
-	if bgImage, err := pictures.GetImage("https://cataas.com/cat", "./assets/default_cat.jpeg"); err != nil {
+	if bgImage, err := pictures.GetImage(configuration.ImageSourceURL, configuration.DefaultImageLocalPath); err != nil {
 		web.GetErrorResponse(w, err)
 	} else {
 		if img, err := pictures.ApplyCaption(bgImage, textAbove, textBelow); err != nil {
